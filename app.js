c5765115820cb3762b9a8903375ca50a62594f0c
skyqiqi <zhao495377793@gmail.com>
2017-10-27 22:22:48 +0800

    test changes

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ var app = new Vue({
         },
         removeTodo: function (todo) {
             let index = this.todoList.indexOf(todo) // Array.prototype.indexOf 是 ES 5 新加的 API
-            this.todoList.splice(index, 1) // 不懂 splice？赶紧看 MDN 文档！
+            this.todoList.splice(index, 1) // splice 查看mdn
         }
     }
 })   
