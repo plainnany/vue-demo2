1836add9b4141a88ae19576aee9b27bad5ef685c
plainnany <plainnany@gmail.com>
2017-11-20 16:44:51 +0800

    delete some comments

--- app.js
+++ app.js
@@ -27,8 +27,8 @@ var app = new Vue({
     created: function () {  //  生命周期钩子函数，可以用来表示在实例创建之后执行
         // onbeforeunload文档：https://developer.mozilla.org/zh-CN/docs/Web/API/Window/onbeforeunload
         window.onbeforeunload = () => {
-            let dataString = JSON.stringify(this.todoList) // JSON 文档: https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/JSON
-            window.localStorage.setItem('myTodos', dataString) // 看文档https://developer.mozilla.org/zh-CN/docs/Web/API/Window/localStorage
+            let dataString = JSON.stringify(this.todoList) 
+            window.localStorage.setItem('myTodos', dataString) 
             let newTodo = JSON.stringify(this.newTodo)
             window.localStorage.setItem('newTodo', newTodo)
         }
